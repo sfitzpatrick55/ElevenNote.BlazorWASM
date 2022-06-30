4ae2a8e461f52cced24bbdd71ac70b01cabe5a09
Scott <sfitzpatrick55@gmail.com>
2022-06-30 19:27:44 -0400

    Create NoteCreate class

---
+++ ElevenNote.BlazorWASM/Shared/Models/NoteCreate.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ElevenNote.BlazorWASM.Shared.Models
+{
+    public class NoteCreate
+    {
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string Content { get; set; }
+
+        public int CategoryId { get; set; }
+    }
+}
+
