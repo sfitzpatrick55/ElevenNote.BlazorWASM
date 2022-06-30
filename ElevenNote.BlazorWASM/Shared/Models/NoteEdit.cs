caa8584fc074a9563a2bacc0483bedbffbd7de99
Scott <sfitzpatrick55@gmail.com>
2022-06-30 19:49:59 -0400

    Create NoteEdit class

---
+++ ElevenNote.BlazorWASM/Shared/Models/NoteEdit.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ElevenNote.BlazorWASM.Shared.Models
+{
+    public class NoteEdit
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string Content { get; set; }
+
+        public int CategoryId { get; set; }
+    }
+}
+
