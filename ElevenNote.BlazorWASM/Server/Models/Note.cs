1c16ca4fcb1214c0a3390f19e33e11ee5c339b92
Scott <sfitzpatrick55@gmail.com>
2022-06-29 20:19:18 -0400

    Add Note model

---
+++ ElevenNote.BlazorWASM/Server/Models/Note.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ElevenNote.BlazorWASM.Server.Models
+{
+    public class Note
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string OwnerId { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string Content { get; set; }
+
+        public int CategoryId { get; set; }
+        public virtual Category Category { get; set; }
+
+        [Required]
+        public DateTimeOffset CreatedUtc { get; set; }
+
+        public DateTimeOffset? ModifiedUtc { get; set; }
+    }
+}
