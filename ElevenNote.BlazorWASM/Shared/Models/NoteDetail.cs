e856e1006cf7a2ecf9218803cb5cfe6e01764d88
Scott <sfitzpatrick55@gmail.com>
2022-06-30 19:48:27 -0400

    Create NoteDetail class

---
+++ ElevenNote.BlazorWASM/Shared/Models/NoteDetail.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace ElevenNote.BlazorWASM.Shared.Models
+{
+    public class NoteDetail
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Content { get; set; }
+
+        public int CategoryId { get; set; }
+
+        public string CategoryName { get; set; }
+
+        public DateTimeOffset CreatedUtc { get; set; }
+
+        public DateTimeOffset? ModifiedUtc { get; set; }
+    }
+}
+
---
+++ ElevenNote.BlazorWASM/Shared/Models/NoteListItem.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace ElevenNote.BlazorWASM.Shared.Models
+{
+    public class NoteListItem
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string CategoryName { get; set; }
+        public DateTimeOffset CreatedUtc { get; set; }
+    }
+}
+
