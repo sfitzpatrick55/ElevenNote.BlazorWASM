d2db2b25e6e0c51b029ab77b3842db0b80aff1dc
Scott <sfitzpatrick55@gmail.com>
2022-06-29 20:20:56 -0400

    Add Category model

---
+++ ElevenNote.BlazorWASM/Server/Models/Category.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ElevenNote.BlazorWASM.Server.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+    }
+}
+
